e03d4b063da24c36d819a89cbf8e44eb75d7412d
Mariusz-Karbowski <m.karbowski@outlook.com>
2022-05-11 20:29:41 +0200

    12.3. Product personalization - components separation

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,16 +1,13 @@
 import styles from './Product.module.scss';
-import clsx from 'clsx';
-import Button from '../Button/Button';
+import ProductImage from '../ProductImage/ProductImage';
+import ProductForm from '../ProductForm/ProductForm';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import shortid from 'shortid';
+
 
 const Product = ({ name, title, colors, sizes, basePrice }) => {  
   const [currentColor, setCurrentColor] = useState(colors[0]);
   const [currentSize, setCurrentSize] = useState(sizes[0]);
-  const colorClassName = color => {
-    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
-  }
   const getPrice = () => {
     return(basePrice + sizes.find((element) => element === currentSize).additionalPrice);
   }
@@ -26,42 +23,21 @@ const Product = ({ name, title, colors, sizes, basePrice }) => {
   }
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt={title}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`} />
-      </div>
+      <ProductImage name={name} title={title} currentColor={currentColor} />
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
           <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
-        <form onSubmit={addToCart}>
-          <div className={styles.sizes}>
-            <h3 className={styles.optionLabel}>Sizes</h3>
-            <ul className={styles.choices}>
-              {sizes.map(size =>
-                <li key={shortid()}>
-                  <button type="button" className={clsx(currentSize === size && styles.active)} onClick={() => setCurrentSize(size)}>{size.name}</button>
-                </li>
-              )}              
-            </ul>
-          </div>
-          <div className={styles.colors}>
-            <h3 className={styles.optionLabel}>Colors</h3>
-            <ul className={styles.choices}>
-              {colors.map(color =>
-                <li key={shortid()}>
-                  <button type="button" className={clsx(colorClassName(color), currentColor === color && styles.active)} onClick={() => setCurrentColor(color)}></button>
-                </li>
-              )}
-            </ul>
-          </div>
-          <Button className={styles.button}>
-            <span className="fa fa-shopping-cart" />
-          </Button>
-        </form>
+        <ProductForm
+          addToCart={addToCart}
+          colors={colors}
+          currentColor={currentColor}
+          setCurrentColor={setCurrentColor}
+          sizes={sizes}
+          currentSize={currentSize}
+          setCurrentSize={setCurrentSize}
+        />
       </div>
     </article>
   )
@@ -73,5 +49,5 @@ Product.propTypes = {
   colors: PropTypes.array.isRequired,
   sizes: PropTypes.array.isRequired,
   basePrice: PropTypes.number.isRequired,
-}
+};
 export default Product;
